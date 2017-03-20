53f1ead360046462b5fd100b0f50044c78a6f92b
Pedro Sampaio <sampaio.pedro@gmail.com>
2017-03-20 22:25:34 +0000

    Setup empty project

---
+++ build.fsx
@@ -0,0 +1,32 @@
+// include Fake lib
+#r "packages/build/FAKE/tools/FakeLib.dll"
+open Fake
+
+RestorePackages()
+
+// Properties
+let buildDir = "./artifacts"
+let solutionFile = "Quincy.sln"
+
+MSBuildDefaults <-{
+    MSBuildDefaults with
+        Verbosity = Some (Quiet)
+}
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir]
+)
+
+Target "BuildApp" (fun _ ->
+    !! solutionFile
+     |> MSBuildRelease buildDir "Build"
+     |> Log "AppBuild-Output: "
+)
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+
+// start build
+RunTargetOrDefault "BuildApp"
