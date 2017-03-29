0d4a1f4f883a467adfc2ee532fcf4166663b3c76
Pedro Sampaio <sampaio.pedro@gmail.com>
2017-03-29 10:38:25 +0100

    Added Caliburn registry

---
+++ src/Quincy/EventAggregatorHandlerRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Caliburn.Micro;
+using StructureMap.Building.Interception;
+using StructureMap.Pipeline;
+using StructureMap.TypeRules;
+
+namespace Quincy
+{
+    public class EventAggregatorHandlerRegistration : IInterceptorPolicy
+    {
+        public string Description => "Adds the constructed object to the EventAggregator";
+
+        public IEnumerable<IInterceptor> DetermineInterceptors(Type pluginType, Instance instance)
+        {
+            if (!instance.ReturnedType.FindInterfacesThatClose(typeof(IHandle<>)).Any()) yield break;
+            yield return new ActivatorInterceptor<IHandle>((c, h) => c.GetInstance<IEventAggregator>().Subscribe(h));
+        }
+    }
+}
