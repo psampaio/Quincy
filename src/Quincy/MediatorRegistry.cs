d31132684fdfc65d7a9b98b7044d187b224d37c5
Pedro Sampaio <sampaio.pedro@gmail.com>
2017-03-29 11:29:43 +0100

    Added MediatR and its registry

---
+++ src/Quincy/MediatorRegistry.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using MediatR;
+using StructureMap;
+
+namespace Quincy
+{
+    public class MediatorRegistry : Registry
+    {
+        public MediatorRegistry(IEnumerable<Assembly> assemblies)
+        {
+            Scan(scanner =>
+            {
+                foreach (var assembly in assemblies)
+                    scanner.Assembly(assembly);
+
+                scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>));
+                scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>));
+                scanner.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>));
+                scanner.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>));
+
+                scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+                scanner.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
+            });
+
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+            For<IMediator>().Use<Mediator>().Singleton();
+        }
+    }
+}
