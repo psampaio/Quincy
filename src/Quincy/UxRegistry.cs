2cf4917e2f7cae6789ff97f33a2a8aae56435321
Pedro Sampaio <sampaio.pedro@gmail.com>
2017-03-29 11:45:27 +0100

    Added the SnackbarMessageQueue registration

--- src/Quincy/UxRegistry.cs
+++ src/Quincy/UxRegistry.cs
@@ -1,4 +1,5 @@
-﻿using Quincy.Progress;
+﻿using MaterialDesignThemes.Wpf;
+using Quincy.Progress;
 using StructureMap;
 
 namespace Quincy
@@ -9,6 +10,11 @@ namespace Quincy
         {
             For<IDialogManager>().Use<DialogManager>().Singleton();
             For<IProgressReporter>().Use<ProgressReporter>().Singleton();
+
+            For<ISnackbarMessageQueue>()
+                .Use<SnackbarMessageQueue>()
+                .SelectConstructor(() => new SnackbarMessageQueue())
+                .Singleton();
         }
     }
 }
