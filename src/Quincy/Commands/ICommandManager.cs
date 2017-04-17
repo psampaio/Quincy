7aae933aa605367a90524efdffd0011ab931b012
Pedro Sampaio <sampaio.pedro@gmail.com>
2017-04-17 09:38:05 +0100

    Removed all the command stuff

--- src/Quincy/Commands/ICommandManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Input;
-
-namespace Quincy.Commands
-{
-    public interface ICommandManager
-    {
-        ICommand Get<TCommand>() where TCommand : IGenerateCommand;
-    }
-}
--- src/Quincy/Commands/IGenerateCommand.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Input;
-
-namespace Quincy.Commands
-{
-    public interface IGenerateCommand
-    {
-        ICommand Generate();
-    }
-}
